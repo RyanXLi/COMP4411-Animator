dd1874d6a307fc749c08b9079ffa8a42970a92cf
Reapor-Yurnero <reapor.yurnero@gmail.com>
2018-04-27 22:28:58 +0800

    imperfect collision system

--- particleSystem.cpp
+++ particleSystem.cpp
@@ -87,6 +87,7 @@ void ParticleSystem::resetSimulation(float t)
     
 	// TODO
 	clearBaked();
+	particle_vec.clear();
 	bake_start_time = -1;
 	bake_end_time = -1;
 	// These values are used by the UI
@@ -104,10 +105,21 @@ void ParticleSystem::computeForcesAndUpdateParticles(float t)
 		if (!isBakedAt(t)) {
 			printf("collision detection!\n");
 			for (auto a = particle_vec.begin(); a != particle_vec.end(); ++a) {
+				// detection between particles
 				for (auto b = particle_vec.begin(); b != particle_vec.end(); ++b) {
-					if ((a->getP() - b->getP()).length() < 0.4) {
-
+					if (a == b) break;
+					double d = (a->getP() - b->getP()).length();
+					if (d > 0.4 && d < 0.5) {
+						Vec3d temp = a->getV();
+						a->setV(0.9*b->getV());
+						b->setV(0.9*temp);
 					}
+				}
+				// with the rigid ground
+				if ((a->getV())*Vec3d(0, 1, 0) < 0 && (a->getP()-Vec3d(0,-3.5,0))*Vec3d(0, 1, 0) < 0.215) {
+					Vec3d Va = a->getV();
+					Vec3d Vnew = { Va[0], -0.95*Va[1],Va[2]};
+					a->setV(Vnew);
 				}
 			}
 			printf("updateParticles!\n");
@@ -167,16 +179,29 @@ void ParticleSystem::spawnParticles(Mat4f CameraM, Mat4f CurrModelM, float currt
 
 	if (isSimulate()) {
 		printf("spawnParticles!\n");
-		Vec3d WorldP(WorldP4[0], WorldP4[1]+ rand() % 80 / 100.0 - 0.4, WorldP4[2] + rand() % 80 / 100.0 - 0.4);
+		//Vec3d WorldP(WorldP4[0], WorldP4[1]+ rand() % 80 / 100.0 - 0.4, WorldP4[2] + rand() % 80 / 100.0 - 0.4);
 		if (!isBakedAt(currt + 1.0f / bake_fps)) {
-			double mass = (rand() % 50) / 5.0 + 0.5;
+			Vec3d WorldP(WorldP4[0], WorldP4[1], WorldP4[2]);
+			//double mass = (rand() % 50) / 5.0 + 0.5;
+			double mass = 5;
 			Particle newP(WorldP, mass);
-			Vec3d velocity(rand()%3+2.0, rand()%100/100.0*2 - 1, rand() % 100*2 / 100.0);
+			//Vec3d velocity(rand()%3+2.0, rand()%100/100.0*2 - 1, rand() % 100*2 / 100.0);
+			Vec3d velocity(5, 0, 0);
 			newP.setV(velocity);
 			for (std::vector<Force*>::iterator it = force_vec.begin(); it != force_vec.end(); ++it) {
 				newP.addForce(*it);
 			}
 			particle_vec.push_back(newP);
+			Vec3d WorldQ(WorldP4[0]+2.5, WorldP4[1], WorldP4[2]);
+			mass = 5;
+			Particle newQ(WorldQ, mass);
+			//Vec3d velocity(rand()%3+2.0, rand()%100/100.0*2 - 1, rand() % 100*2 / 100.0);
+			velocity = { -5, 0, 0 };
+			newQ.setV(velocity);
+			for (std::vector<Force*>::iterator it = force_vec.begin(); it != force_vec.end(); ++it) {
+				newQ.addForce(*it);
+			}
+			particle_vec.push_back(newQ);
 		}
 	}
 }
